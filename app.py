f08c23cdb3676e328814eecb9b73bf0ac2651719
Neal Magee <nem2p@virginia.edu>
2025-12-03 17:07:43 -0500

    Full working verion of app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
-from chalice import Chalice
 import boto3
-from botocore.exceptions import ClientError
 import json
+from chalice import Chalice
+from botocore.exceptions import ClientError
 
 app = Chalice(app_name='s3-events')
 app.debug = True
@@ -11,14 +11,21 @@ app.debug = True
 S3_BUCKET = os.environ.get('APP_BUCKET_NAME', '')
 DYNAMODB_TABLE_NAME = os.environ.get('DYNAMODB_TABLE_NAME', '')
 
+
 @app.on_s3_event(bucket=S3_BUCKET, events=['s3:ObjectCreated:*'], suffix='.json')
 def s3_handler(event):
-    # get the event, pull the file from s3, read it, and insert into DDB
-    pass
+    app.log.debug(f"Received bucket event: {event.bucket}, key: {event.key}")
+    data = get_s3_object(event.bucket, event.key)
+    insert_data_into_dynamodb(data)
+    return data
 
 def get_s3_object(bucket, key):
-    # get the object from s3
-    pass
+    s3 = boto3.client('s3')
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        return json.loads(response['Body'].read().decode('utf-8'))
+    except Exception as e:
+        print(e)
 
 def insert_data_into_dynamodb(data):
     dynamodb = boto3.resource('dynamodb')
@@ -35,11 +42,18 @@ def insert_data_into_dynamodb(data):
         )
         app.log.debug(f"DynamoDB response: {response}")
         return response
-    except ClientError as e:
+    except Exception as e:
         app.log.error(f"Error inserting data into DynamoDB: {e}")
         raise e
 
 @app.route('/access', methods=['GET'])
 def get_access():
-    # return all records from DDB
-    pass
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(DYNAMODB_TABLE_NAME)
+    try:    
+        items = table.scan()['Items']
+        sorted_items = sorted(items, key=lambda x: x['access_time'])
+        return sorted_items
+    except ClientError as e:
+        app.log.error(f"Error scanning DynamoDB table: {e}")
+        raise e
